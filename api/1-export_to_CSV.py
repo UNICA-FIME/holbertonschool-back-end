2d080ac700d54773a8c4270d2d0a63dbdf63eff2
user name <user email>
2022-12-06 02:39:58 +0000

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress
+Using what you did in the task #0, extend your Python
+script to export data in the CSV format.
 """
 import csv
 import json
