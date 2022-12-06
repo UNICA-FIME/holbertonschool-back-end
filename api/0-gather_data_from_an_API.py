74e9e1f2d105c56940001e79af23658c6367140c
user name <user email>
2022-12-06 00:34:11 +0000

    correction error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,17 +11,17 @@ if __name__ == "__main__":
     EMPLOYEE_NAME = None
     TOTAL_NUMBER_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = 0
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(int(argv[1]))
     req = urlopen(url).read().decode("utf-8")
     json_user = json.loads(req)
     EMPLOYEE_NAME = json_user['name']
     url_todos = "https://jsonplaceholder.typicode.com\
-/users/{}/todos".format(argv[1])
+/users/{}/todos".format(int(argv[1]))
     req_todos = urlopen(url_todos).read().decode("utf-8")
     json_todos = json.loads(req_todos)
     TOTAL_NUMBER_OF_TASKS = len(json_todos)
     url_todos_task = "https://jsonplaceholder.typicode.com\
-/users/{}/todos/?completed=true".format(argv[1])
+/users/{}/todos/?completed=true".format(int(argv[1]))
     req_todos_task = urlopen(url_todos_task).read().decode("utf-8")
     json_todos_task = json.loads(req_todos_task)
     NUMBER_OF_DONE_TASKS = len(json_todos_task)
