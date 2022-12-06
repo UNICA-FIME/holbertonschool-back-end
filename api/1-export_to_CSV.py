7b5502c2e01873de6d494d09d6b7cd72ee716f31
user name <user email>
2022-12-06 14:27:01 +0000

    change

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""
-Check student .CSV output of user information
-"""
-
+''' Export data in the CSV format '''
 import csv
-import json
+import requests
 from sys import argv
-from urllib.request import urlopen
 
 
-def for_api():
-    """ obtener informacion de api """
+def get_api():
+    ''' Gather data from an API '''
+    url = 'https://jsonplaceholder.typicode.com/'
+    uid = argv[1]
 
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(int(argv[1]))
-    req = urlopen(url).read().decode("utf-8")
-    json_user = json.loads(req)
-    USER_ID = json_user['id']
-    USERNAME = json_user['name']
+    # get a specific user from users in jsonplaceholder
+    usr = requests.get(url + 'users/{}'.format(uid)).json()
+    # make a query string to get tasks based on user id
+    todo = requests.get(url + 'todos', params={'userId': uid}).json()
 
-    url_todos = "https://jsonplaceholder.typicode.com\
-/users/{}/todos/".format(int(argv[1]))
-    req_todos = urlopen(url_todos).read().decode("utf-8")
-    json_todos = json.loads(req_todos)
-
-    with open(f'{argv[1]}.csv', 'w', encoding='UTF8') as file:
+    with open('{}.csv'.format(uid), 'w') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for item in json_todos:
-            TASK_COMPLETED_STATUS = item['completed']
-            TASK_TITLE = item['title']
-            data = [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE]
-            writer.writerow(data)
+        for employee in todo:
+            user_id = uid
+            username = usr.get('username')
+            task_comp = employee.get('completed')
+            task_title = employee.get('title')
+
+            emp_record = [user_id, username, task_comp, task_title]
+            writer.writerow(emp_record)
 
 
-if __name__ == "__main__":
-    for_api()
+if __name__ == '__main__':
+    get_api()
