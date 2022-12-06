4b80cf63ae170eebb4c9b927513d4702e87c8989
user name <user email>
2022-12-06 03:00:01 +0000

    change doc

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0, extend your Python
-script to export data in the CSV format.
-"""
+"""Using what you did in the task #0, extend your Python"""
 import csv
 import json
 from sys import argv
