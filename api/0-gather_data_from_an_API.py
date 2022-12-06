dbca2eb1eee65754261b8da831508aed0a8e4a9d
user name <user email>
2022-12-06 01:18:18 +0000

    fix chmod

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,10 +4,9 @@ script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 
-
+import json
 from urllib.request import urlopen
 from sys import argv
-import json
 
 
 def import_api():
